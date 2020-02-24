b5206e58770fa4605f2d828b3f05e6592600f78e
Miсhail h2kb Bereza <berezama@gmail.com>
2020-02-24 17:33:02 +0800

    Added enum Operator

--- src/main/java/io/github/h2kb/Calculator.java
+++ src/main/java/io/github/h2kb/Calculator.java
@@ -1,5 +1,7 @@
 package io.github.h2kb;
 
+import io.github.h2kb.enums.Operator;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -7,12 +9,12 @@ import java.util.Scanner;
 
 public class Calculator {
 
-    private String operator;
+    private Operator operator;
 
     private Scanner fileScanner;
 
     public Calculator(String filePath, String operator) throws FileNotFoundException {
-        this.operator = operator;
+        this.operator = Operator.valueOf(operator);
 
         try {
             fileScanner = new Scanner(new File(filePath));
@@ -53,11 +55,11 @@ public class Calculator {
 
     public int calculate() {
         switch (operator) {
-            case "SUM":
+            case SUM:
                 return sum();
-            case "SUB":
+            case SUB:
                 return sub();
-            case "MULT":
+            case MULT:
                 return mult();
             default:
                 System.out.println("Bad operator " + operator + ".");
---
+++ src/main/java/io/github/h2kb/enums/Operator.java
@@ -0,0 +1,7 @@
+package io.github.h2kb.enums;
+
+public enum Operator {
+    SUM,
+    SUB,
+    MULT;
+}
